0970ff4fe969463713353f69dcae189b673f3fc0
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2025-08-08 13:11:01 +0530

    style: adjust spacing and enhance logout button appearance in Header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -106,7 +106,7 @@ const Header: React.FC<HeaderProps> = ({ isAuthenticated = false }) => {
                   </Link>
                   
                   {/* User menu */}
-                  <div className="flex items-center space-x-4 ml-6 ">
+                  <div className="flex items-center space-x-4 ml-24 ">
                     <span className="text-sm text-gray-50">
                       Welcome,
                       <span className='block justify-self-end'>
@@ -115,7 +115,7 @@ const Header: React.FC<HeaderProps> = ({ isAuthenticated = false }) => {
                     </span>
                     <button
                       onClick={handleLogout}
-                      className="text-gray-300 hover:text-red-600 px-3 py-2 rounded-md text-sm font-medium"
+                      className="text-gray-300 text-red-400 hover:cursor-pointer hover:bg-red-400 hover:text-white px-3 py-2 border border-red-400 ml-4 rounded-md text-sm font-medium"
                     >
                       Logout
                     </button>
